99414ba5b3e1d6ca02cccff2bb081c67ea55a441
Harshit Dwivedi <harshithdwivedi@gmail.com>
2016-09-10 14:58:24 +0530

    Add plasma.sh

---
+++ plasma.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export KWIN_USE_BUFFER_AGE=0; kwin --replace &
+# This script performs a full restart on the plasma desktop and saves you the hassle of restarting 
+# your system whenever plasma crashes (the top bar disappears) 
